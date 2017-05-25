698bbc59e770df10096ec0203a8b7247d20855db
Felix Fischbach <felixfischbach@gmx.de>
2017-05-25 14:03:52 +0200

    Add list of neighbors

--- EchoVS3/Node.cs
+++ EchoVS3/Node.cs
@@ -1,18 +1,26 @@
-﻿namespace EchoVS3
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace EchoVS3
 {
     public class Node
     {
+        public List<IPEndPoint> neighbors = new List<IPEndPoint>();
+
         private string _name;
         private uint _size;
         private string _ip;
         private uint _port;
 
         // Connection details to connect to logger
-        private const string LoggerIp = "192.168.2.2";
-        private const uint LoggerPort = 1234;
+        private readonly IPEndPoint _loggerEndPoint = new IPEndPoint(IPAddress.Parse("192.168.2.2"), 1234);
+
+        // Address of the parent node
+        private readonly IPEndPoint _parentNodeEndPoint;
 
         // Sum of the communicated size of received echo messages
         private uint _receivedSize = 0;
+        private uint _numberOfNeighbors;
 
         // Constructor
         public Node(string name, uint size, string ip, uint port)
