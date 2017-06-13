50cc56ae7b878702966d108604914d062a8f0054
Felix Fischbach <felixfischbach@gmx.de>
2017-06-13 13:18:34 +0200

    Reformat Message.cs

--- EchoVS3/Message.cs
+++ EchoVS3/Message.cs
@@ -9,7 +9,15 @@ namespace EchoVS3
     [Serializable]
     public class Message : ISerializable
     {
+        public Type Type { get; set; }
+
+        public uint Number { get; set; }
+
+        public string Data { get; set; }
+
+
         private static readonly BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
 
         public Message(Type type, uint number, string data)
         {
@@ -17,6 +25,7 @@ namespace EchoVS3
             Number = number;
             Data = data;
         }
+
 
         // Constructor to Deserialize
         public Message(SerializationInfo info, StreamingContext context)
@@ -29,12 +38,6 @@ namespace EchoVS3
             Data = (string) info.GetValue($"{nameof(Data)}", typeof(string));
         }
 
-        public Type Type { get; set; }
-
-        public uint Number { get; set; }
-
-        public string Data { get; set; }
-
         // Method so that the serializer knows how to serialize
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
@@ -45,6 +48,7 @@ namespace EchoVS3
             info.AddValue($"{nameof(Number)}", Number);
             info.AddValue($"{nameof(Data)}", Data);
         }
+
 
         public byte[] ToByteArray()
         {
