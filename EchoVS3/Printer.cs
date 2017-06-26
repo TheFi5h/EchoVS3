e90888a046ffa42283bf63ec59d36b036b78e9c4
Felix Fischbach <felixfischbach@gmx.de>
2017-06-26 20:58:47 +0200

    Add Printer for easy formatted printing

---
+++ EchoVS3/Printer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace EchoVS3
+{
+    public static class Printer
+    {
+        /// <summary>
+        /// Prints a single line in the currently set color
+        /// </summary>
+        /// <param name="message">The message to print</param>
+        public static void Print(string message)
+        {
+            Console.Write(message);
+        }
+
+        /// <summary>
+        /// Prints a message in the given color
+        /// </summary>
+        /// <param name="message">The message to print</param>
+        /// <param name="color">The color for the message</param>
+        public static void Print(string message, ConsoleColor color)
+        {
+            ConsoleColor temp = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = temp;
+        }
+
+        /// <summary>
+        /// Prints a message in the currently selected color and ends with a new line
+        /// </summary>
+        /// <param name="message">The message to print</param>
+        public static void PrintLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Prints a message in the given color and ends with a new line
+        /// </summary>
+        /// <param name="message">The message to print</param>
+        /// <param name="color">The color for the message</param>
+        public static void PrintLine(string message, ConsoleColor color)
+        {
+            ConsoleColor temp = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = temp;
+        }
+
+        
+    }
+}
