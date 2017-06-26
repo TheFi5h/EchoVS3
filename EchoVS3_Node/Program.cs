c91a50b3642a03eee85b6e0e170619d42280b8b2
Felix Fischbach <felixfischbach@gmx.de>
2017-06-26 20:59:48 +0200

    Change to printer and adapt changes for cancelable node

--- EchoVS3_Node/Program.cs
+++ EchoVS3_Node/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using EchoVS3;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace EchoVS3_Node
 {
@@ -12,19 +14,26 @@ namespace EchoVS3_Node
         private static int configurationPort = 9999;
 
         private static byte[] _receivedBytes;
-        private static bool _messageReceived = true;
+        private static bool _messageReceived;
 
         static void Main()
         {
             // Start to listen on the configuration port for configuration messages
             var ipEndPoint = new IPEndPoint(IPAddress.Any, configurationPort);
             var udpClient = new UdpClient(ipEndPoint);
-            var udpState = new UdpState(udpClient, ipEndPoint);
+            string input;
+            bool printOnLateFinish = false;
+
+            Printer.Print($"Beginne Empfang von Netzwerkerstellungsnachrichten auf Port {configurationPort}... ");
 
             // Start listening to the configuration port
             udpClient.BeginReceive(ReceiveCallback, new UdpState(udpClient, ipEndPoint));
 
-            // Start listening for keyboard input
+            Printer.Print("OK", ConsoleColor.Green);
+
+            Printer.PrintLine("Für manuelle Erstellung des Knotens beliebige Taste drücken");
+
+            // Start listening for keyboard or network input
             do
             {
                 // Will wait for either a message received or a key pressed
@@ -33,16 +42,15 @@ namespace EchoVS3_Node
             } while (_messageReceived != true && Console.KeyAvailable != true);
 
             // Check what has been triggered
-            if(_messageReceived)
+            if (_messageReceived)
             {
                 // Convert received bytes to an node creation info object
                 var nodeCreationInfo = NodeCreationInfo.FromByteArray(_receivedBytes);
 
                 // Create the node with the given info
                 _node = new Node(nodeCreationInfo);
-
             }
-            else if(Console.KeyAvailable)
+            else if (Console.KeyAvailable)
             {
                 // Stop listening for udp messages
                 // Will trigger the EndReceive method but udp
@@ -74,11 +82,11 @@ namespace EchoVS3_Node
                 nodeCreationInfo.Size = uint.Parse(Console.ReadLine());
 
                 // Get n-times neighbor information
-                while(true)
+                while (true)
                 {
                     // Get ip for neighbor
                     Console.Write("Bitte IP für Nachbar angeben (Hinzufügen beenden mit Enter ohne Eingabe): ");
-                    var input = Console.ReadLine();
+                    input = Console.ReadLine();
 
                     // Check if enter pressed without input
                     if (input == string.Empty)
@@ -92,7 +100,88 @@ namespace EchoVS3_Node
                     nodeCreationInfo.Neighbors.Add(neighborEndPoint);
                 }
 
+                Console.Write($"Erstelle Knoten mit {nodeCreationInfo.Neighbors.Count} Nachbarn... ");
+
+                try
+                {
+                    _node = new Node(nodeCreationInfo);
+                }
+                catch (Exception e)
+                {
+                    Printer.PrintLine("FAIL", ConsoleColor.Red); 
+                    Printer.PrintLine($"Exception message: {e.Message}");
+                    Printer.PrintLine("Beliebige Taste zum Verlassen drücken...");
+
+                    // Wait for keypress
+                    Console.ReadKey();
+                    return;
+                }
+
+                Printer.PrintLine("OK", ConsoleColor.Green);
             }
+
+            Printer.PrintLine("Starte Knoten. STOP/EXIT/QUIT eingeben zum Beenden!");
+            Printer.PrintLine("-----------------------------------------------------------");
+
+            // Run node in task
+            Task nodeTask = Task.Run(() =>
+            {
+                try
+                {
+                    _node.Run();
+                }
+                catch (Exception e)
+                {
+                    Printer.PrintLine($"Exception caught in Program while executing node: {e.Message}", ConsoleColor.Red);
+                    throw;
+                }
+            });
+
+            Task nodeTaskWaiter = nodeTask.ContinueWith(nodeTaskResult =>
+            {
+                if(printOnLateFinish)
+                    Printer.PrintLine("Knoten beendet. Programm kann nun beendet werden.", ConsoleColor.Yellow);
+            });
+
+            // Clear input
+            input = "";
+
+            // Listen for keyboard input
+            do
+            {
+                if (Console.KeyAvailable)
+                {
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Thread.Sleep(200);
+                }
+            } while (input != "STOP" && input != "EXIT" && input != "QUIT" && input != "STAHP");
+
+            Printer.PrintLine("Beende Knoten...");
+
+            // Stop the node
+            _node.Stop();
+
+            Printer.Print("Warte auf Knoten... ");
+
+            // Wait 10 seconds for task to finish
+            if (nodeTask.Wait(TimeSpan.FromSeconds(5)))
+            {                
+                Printer.PrintLine("OK", ConsoleColor.Green);
+                Printer.PrintLine("Beliebige Taste zum Verlassen drücken...");
+            }
+            else
+            {
+                Printer.PrintLine("FAIL", ConsoleColor.Red);
+                Printer.PrintLine("Task wurde noch nicht beendet. Mit beliebiger Taste trotzdem beenden.");
+
+                // Set flag to let task know to print out when the node task finished
+                printOnLateFinish = true;
+            }
+
+            Console.ReadKey();
         }
 
         private static void ReceiveCallback(IAsyncResult result)
@@ -107,7 +196,7 @@ namespace EchoVS3_Node
                 _receivedBytes = udpClient.EndReceive(result, ref ipEndPoint);
                 _messageReceived = true;
             }
-            catch(Exception)
+            catch (Exception)
             {
                 // Catch the exception if the receiving was cancelled from outside
             }
